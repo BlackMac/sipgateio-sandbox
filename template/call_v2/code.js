6327538b12278d531496dbe10c16040bbd0f35f4
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2017-07-06 11:14:29 +0200

    adds call transfer to RTCM

--- template/call_v2/code.js
+++ template/call_v2/code.js
@@ -1,38 +1,48 @@
 var sessionId=null;
 var muted=false;
 var onhold=false;
-function dial() {
+function dial(btn) {
     let numa = $("#number_a_leg").val();
     let numb = $("#number_b_leg").val();
-    sipgate.post("/v2/sessions/calls",
-        {
-            deviceId: "e0", // use a valid register extension
-            caller: numa,
-            callee: numb,
-            callerId: numa
-        },
-        (res) => {
-            sessionId = res.sessionId;
-        });
+    sipgate.post("/v2/sessions/calls", {
+        deviceId: "e0", // use a valid register extension
+        caller: numa,
+        callee: numb,
+        callerId: numa
+    },
+    (res) => {
+        $('#callactions').removeClass("hidden");
+        sessionId = res.sessionId;
+    });
 }
 
-function hangup() {
+function hangup(btn) {
     sipgate.delete("/v2/calls/"+sessionId, (res) => {
-        
+        $('#callactions').addClass("hidden");
     });
 }
 
 function mute(btn) {
     sipgate.put("/v2/calls/"+sessionId+"/muted", {value:!muted}, (res) => {
-        $(btn).toggleClass("active");
+        $(btn).toggleClass("btn-success");
         muted=!muted;
     });
 }
 
 function hold(btn) {
     sipgate.put("/v2/calls/"+sessionId+"/hold", {value:!onhold}, (res) => {
-        $(btn).toggleClass("active");
+        $(btn).toggleClass("btn-success");
         onhold=!onhold;
     });
 }
 
+function transfer(btn, attended) {
+    let number = $("#transfernumber").val();
+    
+    sipgate.post("/v2/calls/"+sessionId+"/transfer", {
+        attended:attended,
+        phoneNumber: number
+    }, (res) => {
+        $('#callactions').addClass("hidden");
+    });
+}
